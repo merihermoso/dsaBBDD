953e3b1cfcfeb509d26bbfe7f687a488771be751
merihermoso <71981693+merihermoso@users.noreply.github.com>
2021-01-07 16:15:05 +0100

    Orders eliminada, i pequeñas mejoras (Inventory, player...)

--- firefighter.sql
+++ firefighter.sql
@@ -21,6 +21,7 @@ INSERT INTO User(username, email, password, birthdate, score, level)  VALUES ('M
 INSERT INTO User(username, email, password, birthdate, score, level)  VALUES ('Sol7','sol@gmail.com','89e0a57deb5e7e483c35f15f28b019cfc998709819fc0bdaf79386a5e7be60c6', '27/10/1990', 200, 1); 
 INSERT INTO User(username, email, password, birthdate, score, level)  VALUES ('Kei8','kei@gmail.com','774bc10a35da95c446b8a6f156f47d93cfddeb35282d89b115a6cc25518ac9b2', '23/06/1988', 15000, 1); 
 
+
 CREATE TABLE Game
 ( id  INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
 dateStart TEXT NOT NULL,
@@ -39,51 +40,6 @@ INSERT INTO Game(dateStart, timeStart, dateEnd, timeEnd, score)  VALUES ('14-12-
 INSERT INTO Game(dateStart, timeStart, dateEnd, timeEnd, score)  VALUES ('15-12-20','15:00','15-12-20','19:00',500);
 INSERT INTO Game(dateStart, timeStart, dateEnd, timeEnd, score)  VALUES ('14-12-20','12:00','14-12-20','13:00',600);
 
-CREATE TABLE UserGame			
-(id_player INTEGER,
- id_game INTEGER,
- FOREIGN KEY (id_player) REFERENCES User (id),
- FOREIGN KEY (id_game) REFERENCES Game(id)
-) ENGINE = InnoDB;
-
-INSERT INTO UserGame VALUES (1,1); 
-INSERT INTO UserGame VALUES (2,2); 
-INSERT INTO UserGame VALUES (2,3); 
-INSERT INTO UserGame VALUES (4,4); 
-INSERT INTO UserGame VALUES (5,5); 
-INSERT INTO UserGame VALUES (6,6); 
-INSERT INTO UserGame VALUES (7,7); 
-INSERT INTO UserGame VALUES (8,8); 
-
-
-CREATE TABLE Orders
-( id  INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-date TEXT NOT NULL,
-time TEXT NOT NULL,
-price INTEGER
-)ENGINE = InnoDB;
-
-INSERT INTO Orders(date, time , price) VALUES ('17-10-20','2:23',20); 
-INSERT INTO Orders(date, time , price) VALUES ('27-10-20','16:21',50); 
-INSERT INTO Orders(date, time , price) VALUES ('07-11-20','14:23',60); 
-INSERT INTO Orders(date, time , price) VALUES ('26-11-20','19:46',10);
-INSERT INTO Orders(date, time , price) VALUES ('23-12-20','18:36',100); 
-INSERT INTO Orders(date, time , price) VALUES ('28-12-20','06:43',600);
-
-CREATE TABLE UserOrder	
-(id_user INTEGER,
- id_order INTEGER,
- FOREIGN KEY (id_user) REFERENCES User (id),
- FOREIGN KEY (id_order) REFERENCES Orders (id)
-) ENGINE = InnoDB;
-
-INSERT INTO UserOrder VALUES (1,6); 
-INSERT INTO UserOrder VALUES (2,5); 
-INSERT INTO UserOrder VALUES (3,4); 
-INSERT INTO UserOrder VALUES (4,3); 
-INSERT INTO UserOrder VALUES (6,2); 
-INSERT INTO UserOrder VALUES (6,1); 
-
 CREATE TABLE Element
 ( id  INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
 name TEXT NOT NULL,
@@ -98,20 +54,15 @@ INSERT INTO Element(name, description, price)  VALUES ('Cuerda huida' ,'Vuelve a
 INSERT INTO Element(name, description, price)  VALUES ('Casco invisible' ,'Hazte invisible 30s',100); 
 INSERT INTO Element(name, description, price)  VALUES ('Pastilla magica' ,'Aumenta la capacidad de vida',75); 
 
-
-CREATE TABLE OrderElement	
-(id_order INTEGER,
+CREATE TABLE UserElement	
+(id_user INTEGER,
  id_element INTEGER,
- FOREIGN KEY (id_order) REFERENCES Orders (id),
+ FOREIGN KEY (id_user) REFERENCES User (id),
  FOREIGN KEY (id_element) REFERENCES Element (id)
 ) ENGINE = InnoDB;
 
-INSERT INTO OrderElement VALUES (1,6); 
-INSERT INTO OrderElement VALUES (2,5); 
-INSERT INTO OrderElement VALUES (3,4); 
-INSERT INTO OrderElement VALUES (4,3); 
-INSERT INTO OrderElement VALUES (5,2); 
-INSERT INTO OrderElement VALUES (6,1); 
+INSERT INTO UserElement VALUES (1,6); 
+INSERT INTO UserElement VALUES (2,5); 
 
 CREATE TABLE Item
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -124,29 +75,73 @@ damage INTEGER
 
 INSERT INTO Item(name , hit, defense, healing, damage) VALUES ('Defense', 3, 4, 5, 6);
 INSERT INTO Item(name , hit, defense, healing, damage) VALUES ('Weapone', 3, 0, 0, 4); 
-INSERT INTO Item(name , hit, defense, healing, damage) VALUES ('Weapone', 1, 0, 0, 6); 
-INSERT INTO Item(name , hit, defense, healing, damage) VALUES ('Defense', 5, 0, 5, 0);
+INSERT INTO Item(name , hit, defense, healing, damage) VALUES ('Gun', 1, 0, 0, 6); 
+INSERT INTO Item(name , hit, defense, healing, damage) VALUES ('Coin', 5, 0, 5, 0);
 
 CREATE TABLE Player
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-name TEXT NOT NULL, 
+status TEXT,
+coins INTEGER,
+score INTEGER,
+numLevel INTEGER,
+speed INTEGER, 
 hit INTEGER,
 defense INTEGER,
 healing INTEGER,
 damage INTEGER
 )ENGINE = InnoDB;
 
-INSERT INTO Player(name , hit, defense, healing, damage) VALUES ('NewPlayer', 3, 3, 3, 3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
+INSERT INTO Player(status, coins, score, numLevel, speed, hit, defense, healing, damage) VALUES ('Playing', 0, 0, 2, 3,3,3,3,3);
 
+CREATE TABLE UserPlayer
+(id_user INTEGER,
+ id_player INTEGER,
+ FOREIGN KEY (id_user) REFERENCES User (id),
+ FOREIGN KEY (id_player) REFERENCES Player(id)
+) ENGINE = InnoDB;
+
+INSERT INTO UserPlayer VALUES (1,1); 
+INSERT INTO UserPlayer VALUES (2,2); 
+INSERT INTO UserPlayer VALUES (2,3); 
+INSERT INTO UserPlayer VALUES (4,4); 
+INSERT INTO UserPlayer VALUES (5,5); 
+INSERT INTO UserPlayer VALUES (6,6); 
+INSERT INTO UserPlayer VALUES (7,7); 
+
+
+CREATE TABLE PlayerGame			
+(id_player INTEGER,
+ id_game INTEGER,
+ FOREIGN KEY (id_player) REFERENCES Player (id),
+ FOREIGN KEY (id_game) REFERENCES Game(id)
+) ENGINE = InnoDB;
+
+INSERT INTO PlayerGame VALUES (1,1); 
+INSERT INTO PlayerGame VALUES (2,2); 
+INSERT INTO PlayerGame VALUES (2,3); 
+INSERT INTO PlayerGame VALUES (4,4); 
+INSERT INTO PlayerGame VALUES (5,5); 
+INSERT INTO PlayerGame VALUES (6,6); 
+INSERT INTO PlayerGame VALUES (7,7); 
 
 CREATE TABLE Inventory 
 (id_player INTEGER,
  id_item INTEGER,
+ quantity INTEGER,
  FOREIGN KEY (id_player) REFERENCES Player (id),
- FOREIGN KEY (id_item) REFERENCES Item (id),
- quantity INTEGER
+ FOREIGN KEY (id_item) REFERENCES Item (id)
 ) ENGINE = InnoDB;
 
+INSERT INTO Inventory VALUES (1,1,1); 
+INSERT INTO Inventory VALUES (1,2,3);
+INSERT INTO Inventory VALUES (2,1,1);
+INSERT INTO Inventory VALUES (2,2,1);
 
 CREATE TABLE Enemy
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -157,11 +152,10 @@ healing INTEGER,
 damage INTEGER
 )ENGINE = InnoDB;
 
-INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Defense', 3, 4, 5, 6);
-INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Weapone', 3, 0, 0, 4); 
-INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Weapone', 1, 0, 0, 6); 
-INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Defense', 5, 0, 5, 0);
-
+INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Enemy1', 1, 2, 5, 1);
+INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Enemy2', 2, 3, 5, 2); 
+INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Enemy3', 3, 4, 5, 3); 
+INSERT INTO Enemy(name , hit, defense, healing, damage) VALUES ('Enemy4', 4, 5, 5, 4);
 
 CREATE TABLE Map
 ( id  INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
@@ -173,3 +167,4 @@ price INTEGER
 INSERT INTO Map(name, description, price)  VALUES ('PrimerMapa' ,'kasjhdkajshk',5); 
 INSERT INTO Map(name, description, price)  VALUES ('SegundoMapa' ,'kasjhdkajshk',150); 
 INSERT INTO Map(name, description, price)  VALUES ('TercerMapa' ,'kasjhdkajshk',10);
+
